afabc646d169d45b07f825bb981e6b0c6b756ab7
crosswordnexus <crosswordnexus@gmail.com>
2017-01-09 11:21:34 -0800

    Improved notepad alert

--- js/crosswords.js
+++ js/crosswords.js
@@ -1244,7 +1244,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 	};
     
     CrossWord.prototype.showNotepad = function() {
-        alert(this.notepad);
+        $("<div title='Notepad'>" + this.notepad + "</div>").dialog();
     }
 
 	CrossWord.prototype.openSettings = function() {
