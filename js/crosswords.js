c9ebac0d3441fd4bc72aaa7a3489f43246a56bae
boisvert42 <boisvert42@gmail.com>
2020-05-05 19:18:51 -0700

    initial commit

--- js/crosswords.js
+++ js/crosswords.js
@@ -989,6 +989,10 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
         if (!this.clues_bottom) {
             this.active_clues = this.clues_top;
             this.inactive_clues = this.clues_top;
+            if (this.selected_cell) {
+                var new_word = this.active_clues.getMatchingWord(this.selected_cell.x, this.selected_cell.y, true);
+                this.setActiveWord(new_word);
+            }
         }
         else if (this.active_clues && this.active_clues.id === CLUES_TOP) {
             // check that there are inactive clues to switch to
@@ -1744,7 +1748,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
             var my_cells_length = my_cells.length;
             for (var i=0; i<my_cells_length; i++) {
                 var my_number = my_cells[i].number;
-                console.log(my_number);
                 if (my_number === null) {continue;}
                 var other_cells = this.number_to_cells[my_number] || [];
                 for (var other_cell of other_cells) {
@@ -2314,13 +2317,39 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     };
 
     // gets word which has cell with specified coordinates
-    CluesGroup.prototype.getMatchingWord = function(x, y) {
-        var i, word_id, word;
+    CluesGroup.prototype.getMatchingWord = function(x, y, change_word=false) {
+        var i, word_id, word, words = [];
         for (i=0; word_id=this.words_ids[i];i++) {
             word = this.crossword.words.hasOwnProperty(word_id) ? this.crossword.words[word_id] : null;
             if (word && word.cells.indexOf(x+'-'+y) >= 0) {
-                return word;
+                words.push(word);
             }
+        }
+        if (words.length == 1) {
+            return words[0];
+        }
+        else if (words.length == 0) {
+            return null;
+        }
+        else {
+            // with more than one word we look for one
+            // that's either current or not
+            for (i=0; i<words.length; i++) {
+                word = words[i];
+                if (change_word) {
+                    if (word.id != this.crossword.selected_word.id) {
+                        return word;
+                    }
+                }
+                else {
+                    if (word.id == this.crossword.selected_word.id) {
+                        return word;
+                    }
+                }
+            }
+            // if we didn't match a word in the above
+            // just return the first one
+            return words[0];
         }
         return null;
     };
