f01d55e6096e08cfa0959712e1df8bc0915d3ccf
boisvert42 <boisvert42@gmail.com>
2020-05-05 20:23:15 -0700

    Change location of function definition for strict mode

--- js/crosswords.js
+++ js/crosswords.js
@@ -397,6 +397,15 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
         this.print_btn = this.root.find('div.cw-buttons-holder div.cw-print');
 
         this.timer_button = this.root.find('div.cw-buttons-holder div.cw-timer');
+        
+        // function to process uploaded files
+        function processFiles(files) {
+            if (files[0].name.endsWith(".puz")) {
+                loadFromFile(files[0], FILE_PUZ).then(parsePUZ_callback, error_callback);
+            } else {
+                loadFromFile(files[0], FILE_JPZ).then(parseJPZ_callback, error_callback);
+            }
+        }
 
         // preload one puzzle
         if (this.config.puzzle_file && this.config.puzzle_file.hasOwnProperty('url') && this.config.puzzle_file.hasOwnProperty('type')) {
@@ -450,15 +459,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
             this.open_button.on('click', function(e) {
                 this.file_input.click();
             }.bind(this));
-            
-            // function to process uploaded files
-            function processFiles(files) {
-                if (files[0].name.endsWith(".puz")) {
-                    loadFromFile(files[0], FILE_PUZ).then(parsePUZ_callback, error_callback);
-                } else {
-                    loadFromFile(files[0], FILE_JPZ).then(parseJPZ_callback, error_callback);
-                }
-            }
 
             this.file_input.on('change', function() {
                 var files = this.file_input[0].files.length ? this.file_input[0].files: null;
