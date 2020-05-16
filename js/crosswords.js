7a5be5627eee72f84a63e9592cc46844a9eb60f9
boisvert42 <boisvert42@gmail.com>
2020-05-15 22:13:55 -0700

    Accept XPZ files

--- js/crosswords.js
+++ js/crosswords.js
@@ -103,7 +103,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
             '<div class="cw-text">or</div>'+
             '<div class="cw-open-button"></div>'+
         '</div>'+
-        '<input type="file" class="cw-open-jpz" accept=".puz,.xml,.jpz">'+
+        '<input type="file" class="cw-open-jpz" accept=".puz,.xml,.jpz,.xpz">'+
     '</div>'+
     '<div class="cw-notepad-icon"><span class="cwtooltip">Notepad</span></div>'+
     '<div class="cw-settings-icon"><span class="cwtooltip">Settings</span></div>'+
