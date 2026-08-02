22efb6cd604a8a8f7b7dde9b2ee806c9e5cb96bb
Alex Boisvert <boisvert42@gmail.com>
2026-08-02 11:31:45 -0700

    remove ducktiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CRITICAL: You MUST change this string (e.g., v3, v4) every time you deploy a new update!
-const CACHE_NAME = "xw-solver-v20260802182453";
+const CACHE_NAME = "xw-solver-v20260802183017";
 
 const ASSETS = [
     "./",
