b6eaa3d3e273c3f97c81cdd84e732e234b07b52a
Alex Boisvert <boisvert42@gmail.com>
2026-08-03 22:02:12 -0700

    break out export

--- src/crosswords.js
+++ src/crosswords.js
@@ -94,6 +94,10 @@ import {
   renumberGrid
 } from './diagramless.js';
 import {
+  printPuzzle,
+  saveAsIpuz
+} from './export.js';
+import {
   IS_MOBILE,
   CONFIGURABLE_SETTINGS,
   STORAGE_KEY,
@@ -1202,45 +1206,11 @@ import {
       }
 
       async printPuzzle(e) {
-        // fill JSXW
-        this.fillJsXw();
-        try {
-          let doc = await this.jsxw.toPDF();
-          doc.autoPrint();
-          // open in a new tab and trigger print dialog
-          const blobUrl = doc.output("bloburl");
-          window.open(blobUrl, "_blank");
-        } catch (err) {
-          console.error("PDF generation failed:", err);
-        }
+        return printPuzzle.call(this, e);
       }
 
       saveAsIpuz(e) {
-        console.log(e);
-        const json = window.ipuz; // this should be a JSON *string*
-
-        // Create a Blob from the text
-        const blob = new Blob([json], { type: "application/json" });
-
-        // Create a temporary <a> element
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-
-        a.href = url;
-        // Try to sanitize the title for a filename
-        let filename1 = this.title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-        if (!filename1) {
-          // if this didn't work, revert to just "puzzle"
-          filename1 = 'puzzle';
-        }
-        const filename = filename1 + '.ipuz';
-        a.download = filename; // filename for the dialog
-
-        // Trigger a click
-        a.click();
-
-        // Cleanup
-        URL.revokeObjectURL(url);
+        saveAsIpuz.call(this, e);
       }
 
       startTimer() {
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CRITICAL: You MUST change this string (e.g., v3, v4) every time you deploy a new update!
-const CACHE_NAME = "xw-solver-v20260804045839";
+const CACHE_NAME = "xw-solver-v20260804050142";
 
 const ASSETS = [
     "./",
